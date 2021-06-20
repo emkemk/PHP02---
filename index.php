02d0f941beb69f6dcb5ef4e0680855f1fd85b092
nagai <emk_730@hotmail.com>
2021-06-20 22:32:28 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>データ登録</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <style>div{padding: 10px;font-size:16px;}</style>
+</head>
+<body>
+
+<!-- Head[Start] -->
+<header>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+    <div class="navbar-header"><a class="navbar-brand" href="select.php">データ一覧</a></div>
+    </div>
+  </nav>
+</header>
+<!-- Head[End] -->
+
+<!-- Main[Start] -->
+<form method="POST" action="insert.php">
+  <div class="jumbotron">
+   <fieldset>
+    <legend>書籍ブックマーク</legend>
+     <label>書籍名：<input type="text" name="name"></label><br>
+     <label>書籍URL：<input type="text" name="url"></label><br>
+     <label>コメント：<textArea name="comments" rows="2" cols="60"></textArea></label><br>
+     <br>
+     <br>
+     <label> <input type="submit" value="送信">
+    </fieldset>
+  </div>
+</form>
+<!-- Main[End] -->
+
+
+</body>
+</html>
---
+++ insert.php
@@ -0,0 +1,39 @@
+<?php
+//1. POSTデータ取得
+$name = $_POST['name'];
+$url = $_POST['url'];
+$comments = $_POST['comments'];
+
+//2. DB接続します
+try {
+  //Password:MAMP='root',XAMPP=''
+  $pdo = new PDO('mysql:dbname=gs_db_02task;charset=utf8;host=localhost','root','root');
+} catch (PDOException $e) {
+  exit('DBConnectError:'.$e->getMessage());
+}
+
+//３．SQL文を用意(データ登録：INSERT)
+$stmt = $pdo->prepare(
+  "INSERT INTO gs_bm_table( id, name, url , comments, indate )
+  VALUES( NULL, :name, :url, :comments, sysdate() )"
+);
+// 4. バインド変数を用意
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':url', $url, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':comments', $comments, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+
+//5 実行
+$status = $stmt->execute();
+
+//6 data登録処理数
+if($status==false){
+  //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+  $error = $stmt->errorInfo();
+  exit("ErroMassage:".$error[2]);
+}else{
+
+//7．index.phpへリダイレクト
+  header('Location: index.php');//ヘッダーロケーション（リダイレクト）
+ 
+} 
+?>
